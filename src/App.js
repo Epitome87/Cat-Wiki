4f919e18c4fb417ea407d851b495024bb1652e09
Matthew McGrath <rubywep@hotmail.com>
2022-04-28 20:55:05 -0700

    Set up routes for Home, Breeds, Breed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Breed, Breeds, Home } from './pages';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/breeds' element={<Breeds />} />
+        <Route path='/breeds/:id' element={<Breed />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
